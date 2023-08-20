cfddc5ac5a82cf92f5e03e75f9dc9ef7bb04f302
hieuhieunguyen <130642924+hieuhieunguyen@users.noreply.github.com>
2023-08-20 23:55:03 +0700

    Delete src/store directory

--- src/store/btDatVe/actionType.js
+++
@@ -1,3 +0,0 @@
-export const HANDLE_CHAIR_BOOKINGS = "HANDLE_CHAIR_BOOKINGS";
-
-export const PAY = "PAY";
--- src/store/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import btDatVeReducer from "./btDatVe/reducer";
-
-export const rootReducer = combineReducers({
-  btDatVe: btDatVeReducer,
-});
